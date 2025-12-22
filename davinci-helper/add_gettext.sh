152e86b5157c9836533e5e3cec280b7afd0d6a30
Bertrand PRESLES <b.presles@collectifenergie.com>
2025-12-22 13:53:08 +0100

    feat(translation): Updated translation scripts

--- davinci-helper/add_gettext.sh
+++ davinci-helper/add_gettext.sh
@@ -2,6 +2,8 @@
 
 # MERGING THE NEW TEMPLATE LINES IN THE TRANSLATION FILE
 msgmerge --update --no-fuzzy-matching locale/it/LC_MESSAGES/davinci-helper.po locale/davinci-helper.pot
+msgmerge --update --no-fuzzy-matching locale/fr/LC_MESSAGES/davinci-helper.po locale/davinci-helper.pot
 
 # DELETING OLD TRANSLATION LINE UNUSED
 msgattrib --no-obsolete locale/it/LC_MESSAGES/davinci-helper.po -o locale/it/LC_MESSAGES/davinci-helper.po
+msgattrib --no-obsolete locale/fr/LC_MESSAGES/davinci-helper.po -o locale/fr/LC_MESSAGES/davinci-helper.po
--- davinci-helper/generate_pot.sh
+++ davinci-helper/generate_pot.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
 find davinci_helper data -name "*.py" -o -name "*.ui" -o -name "*.xml" -o -name "*.desktop" | xargs xgettext -o locale/davinci-helper.pot
 
 # 7) Output finale
-echo "Nuovo file POT generato ex novo: $POT_FILE"
+echo "New POT file generated: $POT_FILE"
